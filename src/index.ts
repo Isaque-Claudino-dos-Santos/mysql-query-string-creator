68e797d746eee2cd248204d96ae5c32a6b0d1ef5
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-23 13:17:19 -0300

    auter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 import QueryMysql from './QueryMysql'
 
-const query = new QueryMysql()
-
-const createTable = query.table
-  .create('users')
-  .column('id', ['int', 'primary key', 'auto_increment'])
-  .column('name', ['varchar(100)', 'not null'])
-  .query()
-
-console.log('\n\n')
-console.log(createTable)
-console.log('\n\n')
-
 export default QueryMysql
