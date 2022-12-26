574682da5bcada4030e14b5cc9ca7cb8672cb2d0
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-25 22:04:28 -0300

    rename

--- __tests__/QueryTable_create.test.ts
+++ __tests__/QueryTable_create.test.ts
@@ -2,37 +2,29 @@ import MysqlQueryString from '../src'
 
 const qs = new MysqlQueryString()
 
-describe('QueryTable tests', () => {
-  test('table name in query', () => {
-    const table = 'foo'
-    const query = qs.table.create(table).end()
+describe('CreateTable', () => {
+  const table = 'foo'
+  const column = 'bar'
+  const attribute = 'baz'
+  const attributes = ['bar', 'baz']
 
+  test('table name in query', () => {
+    const query = qs.table.create(table).end()
     expect(query).toBe(`create table ${table} ()`)
   })
 
   test('column in query', () => {
-    const table = 'foo'
-    const column = 'bar'
     const query = qs.table.create(table).column(column, []).end()
-
     expect(query).toBe(`create table ${table} (${column} )`)
   })
 
   test('attribute column in query', () => {
-    const table = 'foo'
-    const column = 'bar'
-    const attribute = 'baz'
     const query = qs.table.create(table).column(column, [attribute]).end()
-
     expect(query).toBe(`create table ${table} (${column} ${attribute})`)
   })
 
   test('attributes in query', () => {
-    const table = 'foo'
-    const column = 'bar'
-    const attributes = ['bar', 'baz']
     const query = qs.table.create(table).column(column, attributes).end()
-
     expect(query).toBe(
       `create table ${table} (${column} ${attributes[0]} ${attributes[1]})`
     )
