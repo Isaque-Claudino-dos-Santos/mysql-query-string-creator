2647228a2d58fd9e54e1695bd699a9df150b8668
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-26 23:59:23 -0300

    add method insert

--- src/QueryTable/QueryTable.ts
+++ src/QueryTable/QueryTable.ts
@@ -1,7 +1,9 @@
 import AlterTable from './AlterTable'
 import CreateTable from './CreateTable'
+import InsertTable from './InsertTable'
 
 export default class QueryTable {
   readonly create = (table: string) => new CreateTable(table)
   readonly alter = (table: string) => new AlterTable(table)
+  readonly insert = (table: string) => new InsertTable(table)
 }
