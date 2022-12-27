4c923023e34f092c1710219c47e9ffc610901204
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-26 23:03:04 -0300

    create alter table module class

---
+++ src/QueryTable/AlterTable/AlterTableDrop.ts
@@ -0,0 +1,14 @@
+export default class AlterTableDrop {
+  private readonly columns: string[] = []
+
+  constructor(private readonly table: string) {}
+
+  column(column: string): this {
+    this.columns.push(`drop column ${column}`)
+    return this
+  }
+
+  end(): string {
+    return `alter teable ${this.table} ${this.columns.join(', ')}`
+  }
+}
