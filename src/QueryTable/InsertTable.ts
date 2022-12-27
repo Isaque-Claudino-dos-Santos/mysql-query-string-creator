2916b8e12c4f06c4865d4c64144b837985f44ce4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-27 00:07:52 -0300

    add method value and end

--- src/QueryTable/InsertTable.ts
+++ src/QueryTable/InsertTable.ts
@@ -1,3 +1,18 @@
 export default class InsertTable {
+  private readonly columnsKey: string[] = []
+  private readonly columnsValue: string[] = []
+
   constructor(private readonly table: string) {}
+
+  value(columnkey: string, columnValue: string): this {
+    this.columnsKey.push(columnkey)
+    this.columnsValue.push(columnValue)
+    return this
+  }
+
+  end(): string {
+    return `insert into ${this.table} (${this.columnsKey.join(
+      ','
+    )}) values (${this.columnsValue.join(',')})`
+  }
 }
