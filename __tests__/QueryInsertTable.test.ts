e1156b3b8a2df3866917c65093978650e327fe35
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-27 00:30:43 -0300

    script test class

--- __tests__/QueryInsertTable.test.ts
+++ __tests__/QueryInsertTable.test.ts
@@ -4,9 +4,29 @@ const qs = new MysqlQueryString()
 
 describe('Query InsertTable', () => {
   const table = 'foo'
+  const columnKey = 'bar'
+  const columnValue = 'baz'
 
   test('Table name in query', () => {
     const query = qs.table.insert(table).end()
     expect(`insert into ${table} () values ()`).toBe(query)
+  })
+
+  test('Table value in query', () => {
+    const query = qs.table.insert(table).value(columnKey, columnValue).end()
+    expect(`insert into ${table} (${columnKey}) values (${columnValue})`).toBe(
+      query
+    )
+  })
+
+  test('Table value in query', () => {
+    const query = qs.table
+      .insert(table)
+      .value(columnKey, columnValue)
+      .value(columnKey, columnValue)
+      .end()
+    expect(
+      `insert into ${table} (${columnKey},${columnKey}) values (${columnValue},${columnValue})`
+    ).toBe(query)
   })
 })
