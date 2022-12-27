1368e8c1a6097ab2e9e55ffd217aa002fcb440e9
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-12-26 23:02:51 -0300

    add drop table prop

--- src/QueryTable/AlterTable/AlterTable.ts
+++ src/QueryTable/AlterTable/AlterTable.ts
@@ -1,9 +1,13 @@
 import AlterTableAdd from './AlterTableAdd'
 
+import AlterTableDrop from './AlterTableDrop'
+
 export default class AlterTable {
   readonly add: AlterTableAdd
+  readonly drop: AlterTableDrop
 
   constructor(private readonly table: string) {
     this.add = new AlterTableAdd(table)
+    this.drop = new AlterTableDrop(table)
   }
 }
